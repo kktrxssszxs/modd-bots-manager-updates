88791f81609c736824e404ae5aee6ce99e202021
kktrxssszxs <kktrx200k@gmail.com>
2026-02-27 23:18:58 -0300

    Update core_logic.obf.js

--- core_logic.obf.js
+++ core_logic.obf.js
@@ -4,6 +4,7 @@ module.exports = async function main(deps) {
     try { require('events').EventEmitter.defaultMaxListeners = 0; process.setMaxListeners(0); } catch {}
 
     const VERSION = "1.8.9 Patch-16";
+    // updated disc webhook, and added minor changes (old comment)
     const BASE_DIR = process.pkg ? path.dirname(process.execPath) : process.cwd();
     const PROFILES_DIR = path.join(BASE_DIR, "bot_profiles");
     const STATE_FILE = path.join(BASE_DIR, "session_state.json");

01d4e83b560e592c3b1ab1bfb0b2fefefecb6b6b
kktrxssszxs <kktrx200k@gmail.com>
2026-02-28 22:39:22 -0300

    Update core_logic.obf.js

--- core_logic.obf.js
+++ core_logic.obf.js
@@ -4,6 +4,7 @@ module.exports = async function main(deps) {
     try { require('events').EventEmitter.defaultMaxListeners = 0; process.setMaxListeners(0); } catch { }
 
     const VERSION = "2.1.0";
+    //letitgonbiithletmeflowwhateverheneedstodoljustlemmego
     const BASE_DIR = process.pkg ? path.dirname(process.execPath) : process.cwd();
     const PROFILES_DIR = path.join(BASE_DIR, "bot_profiles");
     const STATE_FILE = path.join(BASE_DIR, "session_state.json");
